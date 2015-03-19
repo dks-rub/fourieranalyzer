e4c4783f376bb147a0d92ee82a2c21324ba9129a
Max <max-hoffmann@gmx.de>
2015-03-19 19:10:38 +0100

    Added Documentation

--- DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/fft/KJFFT.java
+++ DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/fft/KJFFT.java
@@ -2,9 +2,7 @@ package de.rub.dks.fft;
 
 /**
  * @author Kris
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * KJFFT class from the depths of the internet.
  */
 public class KJFFT {
 
