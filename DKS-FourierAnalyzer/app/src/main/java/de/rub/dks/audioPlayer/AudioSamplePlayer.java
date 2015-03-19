d1cd77a825f579716b1b51a2bc1fddd120614b9f
Max <max-hoffmann@gmx.de>
2015-03-19 19:04:25 +0100

    Added Documentation

--- DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/audioPlayer/AudioSamplePlayer.java
+++ DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/audioPlayer/AudioSamplePlayer.java
@@ -7,17 +7,32 @@ import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 
+/*
+* Audio player class to play back arrays of audio bytes.
+* All byte arrays passed to the player will be played in order of insertion until all tasks are done, or a new byte array list is passed.
+*/
 public class AudioSamplePlayer {
 	private Thread thread;
 	private int bufferSize, sampleRate;
 	private AudioPlaybackListener listener;
 
+	/**
+	* Constructor.
+	* Creates a new AudioSamplePlayer which will notify the given listener and use the provided sample rate.
+	* The minimal buffer size for this sample rate will be automatically calculated.
+	* @param listener a listener which will be notified, must not be null!
+	* @param sampleRate the players audio sample rate
+	*/
 	public AudioSamplePlayer(AudioPlaybackListener listener, int sampleRate) {
 		this.sampleRate = sampleRate;
 		this.listener = listener;
 		bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
 	}
 
+	/**
+	* Plays the given byte arrays. If there is already audio playing it will be stopped.
+	* @param audioData the byte arrays to be played
+	*/
 	public void play(final ArrayList<byte[]> audioData) {
 		if (thread != null)
 			stop();
@@ -41,6 +56,17 @@ public class AudioSamplePlayer {
 		};
 		thread.start();
 	}
+	
+	/**
+	* Stops the current playback (if available).
+	*/
+	public void stop() {
+		if (thread != null)
+			thread.interrupt();
+		thread = null;
+	}
+	
+	// getters
 
 	public int getBufferSize() {
 		return bufferSize;
@@ -48,12 +74,6 @@ public class AudioSamplePlayer {
 
 	public int getSampleRate() {
 		return sampleRate;
-	}
-
-	public void stop() {
-		if (thread != null)
-			thread.interrupt();
-		thread = null;
 	}
 
 }
