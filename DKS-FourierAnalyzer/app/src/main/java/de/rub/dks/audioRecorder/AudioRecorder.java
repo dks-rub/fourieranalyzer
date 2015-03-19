5e4197f8527df1d30effc11c8457e4c790a2ad31
Max <max-hoffmann@gmx.de>
2015-03-19 19:09:34 +0100

    Added Documentation

--- DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/audioRecorder/AudioRecorder.java
+++ DKS-FourierAnalyzer/app/src/main/java/de/rub/dks/audioRecorder/AudioRecorder.java
@@ -4,17 +4,35 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
 
+
+/**
+* Audio recorder class to get audio bytes from the devices microphone.
+* A consumer will be notified whenever new audio bytes are available.
+* 
+* REQUIRES permission android.permission.RECORD_AUDIO
+*/
 public class AudioRecorder {
 	private AudioRecordListener consumer;
 	private Thread thread;
 	private int bufferSize, sampleRate;
 
+	/**
+	* Constructor.
+	* Creates a new AudioRecorder which will notify the given consumer and use the provided sample rate.
+	* The minimal buffer size for this sample rate will be automatically calculated.
+	* @param consumer a consumer which will be notified when new audio bytes are available, must not be null!
+	* @param sampleRate the players audio sample rate
+	*/
 	public AudioRecorder(AudioRecordListener consumer, int sampleRate) {
 		this.consumer = consumer;
 		this.sampleRate = sampleRate;
 		bufferSize = AudioRecord.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
 	}
 
+	/**
+	* (Re-)starts recording from the microphone.
+	* Recording lasts until stop() is called.
+	*/
 	public void start() {
 		if (thread != null)
 			stop();
@@ -41,7 +59,18 @@ public class AudioRecorder {
 		};
 		thread.start();
 	}
-
+	
+	/**
+	* Stops recording.
+	*/
+	public void stop() {
+		if (thread != null)
+			thread.interrupt();
+		thread = null;
+	}
+	
+	// getters
+	
 	public int getBufferSize() {
 		return bufferSize;
 	}
@@ -49,11 +78,7 @@ public class AudioRecorder {
 	public int getSampleRate() {
 		return sampleRate;
 	}
+	
 
-	public void stop() {
-		if (thread != null)
-			thread.interrupt();
-		thread = null;
-	}
 
 }
